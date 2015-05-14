fed4e9af82662e6e0fbf3da6d3310547296a139b
AndyMinnie <andrea.cauchoix@gmail.com>
2015-05-14 14:27:52 -0400

    Mise en place interface graphique connexion
    Le traitement n'est pas encore effectué, pour passer à l'interface de
    proposition de défi il suffit de cliquer sur le bouton "sign in"

--- app/src/main/java/com/example/quentin/cyoti/MainActivityFragment.java
+++ app/src/main/java/com/example/quentin/cyoti/MainActivityFragment.java
@@ -2,12 +2,14 @@ package com.example.quentin.cyoti;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
@@ -23,6 +25,7 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
         Button btnConnection = (Button)rootView.findViewById(R.id.buttonConnection);
 
         btnConnection.setOnClickListener(new View.OnClickListener(){
