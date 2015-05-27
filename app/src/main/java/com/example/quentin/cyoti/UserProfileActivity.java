c505ba0a2fbb8acd8e73c7cdf18ca70a3d1f909c
TiLodoss <vincent.aunai@gmail.com>
2015-05-27 16:55:43 -0400

    MAJ profil utilisateur - V1.3
    Suppression du profil : OK
    Création de popups pour sauvegarde/suppression : OK
    
    Reste à changer l'image du profil (pour un compte sans réseau social)

--- app/src/main/java/com/example/quentin/cyoti/UserProfileActivity.java
+++ app/src/main/java/com/example/quentin/cyoti/UserProfileActivity.java
@@ -1,6 +1,8 @@
 package com.example.quentin.cyoti;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -11,6 +13,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.Toast;
+
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
@@ -22,7 +27,18 @@ public class UserProfileActivity extends ActionBarActivity {
     private EditText etUserName;
     private EditText etUserEmail;
     private EditText etUserPassword;
+    private Switch swNotificationsPush;
+    private Switch swMailingNotifications;
+
     private ParseUser currentUser;
+
+    private boolean saveChanges = false;
+    private boolean deleteProfile = false;
+    private boolean notificationsPush;
+    private boolean mailingNotifications;
+
+    private final static int ACTION_SAVE_CHANGES   = 1;
+    private final static int ACTION_DELETE_PROFILE = 2;
 
     public UserProfileActivity() {
         currentUser = ParseUser.getCurrentUser();
@@ -50,16 +66,40 @@ public class UserProfileActivity extends ActionBarActivity {
         });
 
         etUserName = (EditText) this.findViewById(R.id.et_userName);
-        etUserName.setHint(currentUser.getUsername().toString());
+        etUserName.setText(currentUser.getUsername());
 
         etUserEmail = (EditText) this.findViewById(R.id.et_userEmail);
-        etUserEmail.setHint(currentUser.getEmail().toString());
+        etUserEmail.setText(currentUser.getEmail());
 
         etUserPassword = (EditText) this.findViewById(R.id.et_userPwd);
-        etUserPassword.setHint(currentUser.getString("password"));
+        etUserPassword.setText(currentUser.getString("password"));
 
+        swNotificationsPush = (Switch) this.findViewById(R.id.sw_notification);
+        swNotificationsPush.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (swNotificationsPush.isChecked()) notificationsPush = true;
+                else notificationsPush = false;
+            }
+        });
 
-        //AlertDialog alertSave = new AlertDialog();
+        swMailingNotifications = (Switch) this.findViewById(R.id.sw_mailNotification);
+        swMailingNotifications.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (swMailingNotifications.isChecked()) mailingNotifications = true;
+                else mailingNotifications = false;
+            }
+        });
+
+        ImageButton btDeleteProfile = (ImageButton) this.findViewById(R.id.bt_deleteProfile);
+        btDeleteProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAlertDialog(ACTION_DELETE_PROFILE);
+            }
+        });
+
     }
 
     @Override
@@ -87,17 +127,128 @@ public class UserProfileActivity extends ActionBarActivity {
     @Override
     public void onStop() {
         super.onStop();
-        ParseQuery<ParseObject> queryUser = ParseQuery.getQuery("_User");
 
-        try {
-            ParseObject newUser = queryUser.get(currentUser.getObjectId());
+        if (saveChanges) {
+            ParseQuery<ParseObject> queryUser = ParseQuery.getQuery("_User");
 
-            newUser.put("username", etUserName.getText().toString());
-            newUser.put("email", etUserEmail.getText().toString());
-            newUser.save();
-        } catch (ParseException e) {
-            e.printStackTrace();
-            Log.d("majU", "Mise à jour du user échouée");
+            try {
+                ParseObject newUser = queryUser.get(currentUser.getObjectId());
+
+                newUser.put("username", etUserName.getText().toString());
+                newUser.put("email", etUserEmail.getText().toString());
+                newUser.put("password", etUserPassword.getText().toString());
+                newUser.put("newsletter", mailingNotifications);
+
+                /* TODO : ajouter champ notificationPush dans BDD */
+
+                newUser.save();
+            } catch (ParseException e) {
+                e.printStackTrace();
+
+                Toast.makeText(getApplicationContext(),
+                                "Error ! Please try again later.",
+                                Toast.LENGTH_SHORT).show();
+
+                Log.d("majU", "Mise à jour du user échouée");
+            }
+        }
+
+        else if(deleteProfile) {
+            ParseQuery<ParseObject> queryUser = ParseQuery.getQuery("_User");
+
+            try {
+
+                // Removing informations about the user
+                ParseObject newUser = queryUser.get(currentUser.getObjectId());
+                newUser.remove("password");
+                newUser.remove("email");
+                newUser.remove("experience");
+                newUser.remove("facebook_account");
+                newUser.remove("friend_list");
+                newUser.remove("level");
+                newUser.remove("mail_checking");
+                newUser.remove("newsletter");
+                newUser.remove("twitter_account");
+                newUser.remove("createdAt");
+                newUser.remove("updatedAt");
+                newUser.remove("ACL");
+
+                // Freezing the account
+                newUser.put("freeze_account", true);
+
+                // Updating account
+                newUser.save();
+
+
+                // Show connection activity
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            }
+
+            catch (ParseException e) {
+                e.printStackTrace();
+
+                Toast.makeText(getApplicationContext(),
+                        "Error ! Please try again later.",
+                        Toast.LENGTH_SHORT).show();
+
+                Log.d("delU", "Suppression du user échouée");
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        showAlertDialog(ACTION_SAVE_CHANGES);
+    }
+
+    public void showAlertDialog(int action) {
+        switch (action) {
+            case ACTION_SAVE_CHANGES:
+                AlertDialog.Builder alertSave = new AlertDialog.Builder(this);
+                alertSave.setTitle("Save changes ?");
+                alertSave.setMessage(R.string.tv_saveChanges);
+
+                alertSave.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        saveChanges = true;
+                        finish();
+                    }
+                });
+
+                alertSave.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+
+                alertSave.show();
+
+                break;
+
+            case ACTION_DELETE_PROFILE:
+                AlertDialog.Builder alertDelete = new AlertDialog.Builder(this);
+                alertDelete.setTitle("Delete profile ?");
+                alertDelete.setMessage(R.string.tv_deleteProfile);
+
+                alertDelete.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        deleteProfile = true;
+                        finish();
+                    }
+                });
+
+                alertDelete.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+
+                alertDelete.show();
         }
     }
 }
