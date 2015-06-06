9b78e0978d646636f780fed965685175f0ae525d
vallereaugabriel <vallereaugabriel@hotmail.fr>
2015-06-06 16:59:07 -0400

    Commit simple

---
+++ app/src/main/java/com/example/quentin/cyoti/utilities/DescriptionChallengeActivity.java
@@ -0,0 +1,15 @@
+package com.example.quentin.cyoti.utilities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Gabriel on 05/06/2015.
+ */
+public class DescriptionChallengeActivity extends AppCompatActivity {
+
+    public DescriptionChallengeActivity () {
+
+    }
+
+
+}
