6de154d626e5d1c3c13db8ce45fb3526c211945b
AndyMinnie <andrea.cauchoix@gmail.com>
2015-06-11 18:06:52 -0400

    Liste Amis dans l'alert Dialog
    Il reste maintenant l'ajout en base à faire :+1:

--- app/src/main/java/com/example/quentin/cyoti/VoteChallengeFragment.java
+++ app/src/main/java/com/example/quentin/cyoti/VoteChallengeFragment.java
@@ -1,24 +1,35 @@
 package com.example.quentin.cyoti;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.quentin.cyoti.adapters.FriendAdapter;
 import com.example.quentin.cyoti.metier.Challenge;
 import com.example.quentin.cyoti.metier.Friend;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class VoteChallengeFragment extends Fragment {
@@ -26,9 +37,22 @@ public class VoteChallengeFragment extends Fragment {
     private Friend friendChallenged;
     private Challenge challenge = new Challenge();
     private Double percentVote = 0.0;
-    private View rootView;
     private ProgressBar vote;
+    private View rootView;
+
+    //getfriends
     private TextView tvPercent;
+    private ParseObject tempObject;
+    private ParseUser currentUser;
+    private String friends;
+
+    List<String> listFriends;
+    ArrayList<String> names;
+
+    public VoteChallengeFragment(){
+        currentUser = ParseUser.getCurrentUser();
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -44,41 +68,86 @@ public class VoteChallengeFragment extends Fragment {
         btnPropose.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View arg0) {
-                // Creating alert Dialog with two Buttons
-
+                //initialisation alertdialog
                 AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+                LayoutInflater inflater = getActivity().getLayoutInflater();
+                final View convertView = (View) inflater.inflate(R.layout.custom, null);
+                alertDialog.setView(convertView);
 
-                // Setting Dialog Title
+                //setting a title for the dialog
                 alertDialog.setTitle(R.string.dialog_title);
 
-                // Setting Dialog Message
-                alertDialog.setMessage(R.string.dialog_message);
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("_User");
+                String userID = currentUser.getObjectId().toString();
+                query.getInBackground(userID, new GetCallback<ParseObject>() {
+                    public void done(ParseObject object, ParseException e) {
+                        if (e == null) {// pas d'exceptions
+                            listFriends = new ArrayList<String>();
+                            listFriends = object.getList("friend_list");
+                            if (listFriends != null) {
+                                names = new ArrayList<String>();
+                                for (int i = 0; i < listFriends.size(); i++) {
+                                    String temp = listFriends.get(i);
+                                    names.add(temp);
+                                }
+
+                                final ListView lv = (ListView) convertView.findViewById(R.id.listView1);
+                                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, names);
+                                lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                    @Override
+                                    public void onItemClick(AdapterView<?> parent, View view, int position,
+                                                            long id) {
+                                        friends = (String) lv.getItemAtPosition(position);
+                                        Log.e("TAG", friends.toString());
+                                    }
+                                });
+                                lv.setAdapter(adapter);
+                                Log.d("GET FRIENDS", listFriends.toString());
+                            }
+                            else{
+                                Toast.makeText(getActivity().getApplicationContext(),"you don't have friends yet", Toast.LENGTH_LONG);
+                            }
+                        } else {
+                            Log.d("GET FRIENDS", "an error has occured");
+                        }
+                    }
+                });
+
+
 
                 // Setting Positive "Yes" Button
                 alertDialog.setPositiveButton("YES",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,int which) {
+                        new DialogInterface.OnClickListener()
+
+                        {
+                            public void onClick(DialogInterface dialog, int which) {
                                 // Write your code here to execute after dialog
-                                Toast.makeText(getActivity().getApplicationContext(),R.string.yes_propose, Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getActivity().getApplicationContext(), R.string.yes_propose, Toast.LENGTH_SHORT).show();
                             }
-                        });
+                        }
+
+                );
                 // Setting Negative "NO" Button
                 alertDialog.setNegativeButton("NO",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,	int which) {
+                        new DialogInterface.OnClickListener()
+
+                        {
+                            public void onClick(DialogInterface dialog, int which) {
                                 // Write your code here to execute after dialog
                                 Toast.makeText(getActivity().getApplicationContext(), R.string.no_propose, Toast.LENGTH_SHORT).show();
                                 dialog.cancel();
                             }
-                        });
+                        }
+
+                );
 
                 // Showing Alert Message
                 alertDialog.show();
 
             }
         });
-
-        if(challenge.getCreatedDate() != null) {
+                /* FIN ALERTDIALOG*/
+        if (challenge.getCreatedDate() != null) {
 
             TextView descriptionChallenge = (TextView) rootView.findViewById(R.id.tv_descriptionChallenge);
             descriptionChallenge.setText(friendChallenger.getFirstName()
@@ -186,7 +255,8 @@ public class VoteChallengeFragment extends Fragment {
         ParseQuery<ParseObject> queryUserApplicant = ParseQuery.getQuery("_User");
         queryUserApplicant.whereEqualTo("objectId", userChallengerID);
 
-        ParseQuery<ParseObject> queryUser = ParseQuery.getQuery("_User");;
+        ParseQuery<ParseObject> queryUser = ParseQuery.getQuery("_User");
+        ;
         queryUser.whereEqualTo("objectId", userChallengedID);
 
         try {
@@ -233,7 +303,7 @@ public class VoteChallengeFragment extends Fragment {
 
     public boolean hasVoted() {
         boolean hasVoted;
-        ParseQuery<ParseObject> queryUser= ParseQuery.getQuery("Vote");
+        ParseQuery<ParseObject> queryUser = ParseQuery.getQuery("Vote");
         queryUser.whereEqualTo("attributed_challenge_id", "idTest");
         queryUser.whereEqualTo("user_id", ParseUser.getCurrentUser().getObjectId());
 
@@ -243,13 +313,11 @@ public class VoteChallengeFragment extends Fragment {
 
             if (userID.equals(ParseUser.getCurrentUser().getObjectId())) {
                 Toast.makeText(getActivity().getApplicationContext(),
-                                "You have already voted for this challenge !",
-                                Toast.LENGTH_SHORT).show();
+                        "You have already voted for this challenge !",
+                        Toast.LENGTH_SHORT).show();
 
                 hasVoted = true;
-            }
-
-            else hasVoted =  false;
+            } else hasVoted = false;
         } catch (ParseException e) {
             e.printStackTrace();
             hasVoted = false;
